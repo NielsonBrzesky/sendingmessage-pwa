19efaba9d6a3ccd7bcf25cc64cafd3bc4dd27605
Nielson Brzesky <nilbrzesky@outlook.com>
2024-08-28 08:46:11 -0300

    Teste no caminho do service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,148 @@
-// Escuta o evento de push: Quando uma notificação push é recebida, exibe uma notificação com um título e corpo.
-self.addEventListener('push', function (event) {
-  console.log('TESTE DE PUSH: NOTIFICAÇÃO RECEBIDA! ', event)
+import CryptoJS from 'crypto-js'
 
-  let data = {}
-  if (event.data) {
-    try {
-      data = event.data.json() // Tenta analisar os dados como JSON
-      console.log('DATA: ', data)
-    } catch (e) {
-      console.error('Erro ao analisar os dados da notificação:', e)
-      data = { title: 'Notificação', body: 'Você tem uma nova mensagem.' }
-    }
-  } else {
-    data = { title: 'Notificação', body: 'Você tem uma nova mensagem.' }
-  }
+const publicVapidKey =
+  'BOc4XoWWX6ggheu3H-HYmkmnPA4wX5np-TqUuIcfqilxt3UCHWPUMB1ch36jYBoeuJ6yJxWeEVvzGVg07dkEoZM'
 
-  const options = {
-    body: data.message,
-    icon: 'path/to/icon.png',
-    badge: 'path/to/badge.png'
-  }
-  event.waitUntil(
-    self.registration.showNotification(data.title, options).then(() => {
-      // Envia a mensagem para a aplicação principal
-      self.clients.matchAll().then((clients) => {
-        clients.forEach((client) => {
-          client.postMessage({
-            type: 'PUSH_NOTIFICATION',
-            data: data
-          })
+// Solicita permissão para notificações
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    // Verifica se o navegador suporta Service Workers
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(async (registration) => {
+          console.log('Service Worker registrado com sucesso:', registration)
+
+          // Verifica se o navegador suporta Push Manager
+          if (!('PushManager' in window)) {
+            throw new Error('Push messaging não é suportado.')
+          }
+
+          // Verificar se já existe uma inscrição
+          const existingSubscription = await registration.pushManager.getSubscription()
+          if (existingSubscription) {
+            // Cancelar a inscrição existente
+            await existingSubscription.unsubscribe()
+            console.log('Inscrição existente cancelada.')
+          }
+
+          try {
+            // Inscrever-se novamente com a nova chave
+            const subscription = await registration.pushManager.subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+            })
+
+            // Verificar se os campos Auth e P256DH estão presentes
+            const subscriptionJson = subscription.toJSON()
+            console.log('Subscription JSON:', subscriptionJson) // Adicionar log para inspecionar o conteúdo
+
+            // Tenta analisar a resposta como JSON
+            try {
+              const parsedResponse = JSON.parse(JSON.stringify(subscriptionJson))
+              console.log('Inscrição de Push Manager:', parsedResponse)
+            } catch (jsonError) {
+              console.error('Erro ao analisar a resposta JSON:', jsonError)
+              console.error('Resposta recebida:', subscriptionJson)
+            }
+
+            if (
+              !subscriptionJson.keys ||
+              !subscriptionJson.keys.auth ||
+              !subscriptionJson.keys.p256dh
+            ) {
+              throw new Error('A inscrição não contém os campos necessários.')
+            }
+
+            // Gerar um installationId único
+            const installationId = generateUUID()
+
+            // Ajustar a estrutura do JSON para o formato esperado pelo back-end
+            const formattedSubscriptionJSON = {
+              installationId: installationId,
+              platform: 'browser',
+              pushChannel: {
+                endpoint: subscriptionJson.endpoint,
+                p256dh: subscriptionJson.keys.p256dh,
+                auth: subscriptionJson.keys.auth
+              }
+            }
+
+            // Enviar a inscrição para o back-end
+            await sendSubscriptionToBackend(formattedSubscriptionJSON)
+          } catch (error) {
+            console.error('Erro ao inscrever-se no Push Manager:', error)
+          }
         })
-      })
-    })
-  )
+        .catch((error) => {
+          console.error('Erro ao registrar o Service Worker:', error)
+        })
+    }
+  }
 })
 
-// Escuta o evento de clique na notificação: Quando o usuário clica na notificação, a aba do navegador é aberta em uma URL específica.
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close()
-  event.waitUntil(
-    self.clients.openWindow('https://coden3.com.br/') // Colocar URL desejada
-  )
-})
+// Converte uma string Base64 URL-safe em um array de bytes (Uint8Array)
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+// Gera um token de assinatura de acesso compartilhado (SAS) para autenticação.
+function getSelfSignedToken(connectionString, expiryMinutes) {
+  const [endpoint, sasKeyName, sasKeyValue] = connectionString
+    .split(';')
+    .map((part) => part.split('=')[1])
+  const expiry = Math.floor(new Date().getTime() / 1000) + expiryMinutes * 60
+  const stringToSign = encodeURIComponent(endpoint) + '\n' + expiry
+  const hmac = CryptoJS.HmacSHA256(stringToSign, sasKeyValue)
+  const signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hmac))
+  return `SharedAccessSignature sr=${endpoint}&sig=${signature}&se=${expiry}&skn=${sasKeyName}`
+}
+
+// Função para gerar UUID
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
+
+// Função para enviar a inscrição para o back-end
+async function sendSubscriptionToBackend(subscription) {
+  console.log('Enviando inscrição para o back-end:', subscription)
+
+  // Gerar o token de autenticação
+  const connectionString =
+    'Endpoint=sb://sendingmessagesnotification.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=0mldQ0+u3uvR3QxnG8+YZS5OzI2MhPod9Ey5Fn6rU='
+  const token = getSelfSignedToken(connectionString, 60) // 60 minutos de validade
+
+  // Enviar a inscrição para o servidor com autenticação
+  try {
+    const response = await fetch(
+      'https://8a7f-2804-11e4-423-6800-3894-8e16-9083-163c.ngrok-free.app/api/Notification/subscribe',
+      {
+        method: 'POST',
+        body: JSON.stringify(subscription),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error('Erro ao enviar a inscrição para o servidor.')
+    }
+
+    console.log('Inscrição enviada com sucesso para o servidor.')
+  } catch (error) {
+    console.error('Erro ao enviar a inscrição para o servidor:', error)
+  }
+}
