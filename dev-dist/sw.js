e1308935f9b8f29e7d1f353268779c160f8120db
Nielson Brzesky <nilbrzesky@outlook.com>
2024-08-28 08:33:15 -0300

    Ultima modificacão com a url para teste.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kjhlsq8od6"
+    "revision": "0.nsc6e6adcig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
